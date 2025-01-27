26b659c92253ccafe9bd2b2fbb23fa0bb1346d38
lovakumar12 <kumarlova078@gmail.com>
2025-01-27 09:38:56 +0530

    adding model_trainer

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -21,3 +21,19 @@ class DataTransformationArtifact:
     transformed_object_file_path:str
     transformed_train_file_path:str
     transformed_test_file_path:str
+    
+    
+# code for artifact entity for model trainer
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str
+    metric_artifact:ClassificationMetricArtifact
--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -6,9 +6,12 @@ from src.logger import logging
 from src.components.data_ingenstion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
-from src.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
-from src.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact)
+from src.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,
+                                      ModelTrainerConfig)
+from src.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact,
+                                        ModelTrainerArtifact)
 
 
 
@@ -17,6 +20,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
 
@@ -78,6 +82,19 @@ class TrainPipeline:
         except Exception as e:
             raise Cardeo_risk_Exception(e, sys)
 
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise Cardeo_risk_Exception(e, sys)
 
 
 
@@ -97,7 +114,7 @@ class TrainPipeline:
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
             logging.info("Data Transformation is done")
 
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
             # model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
             #                                                         model_trainer_artifact=model_trainer_artifact)
